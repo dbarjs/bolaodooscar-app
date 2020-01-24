1f1756c018e802483b359634826dad284052ceba
Eduardo Barros <joseeduardobarros@gmail.com>
2020-01-24 16:41:48 -0300

    Improve VoteListItem

--- components/VoteListItem.vue
+++ components/VoteListItem.vue
@@ -1,6 +1,9 @@
 <template>
   <v-col cols="12" v-if="vote">
     <v-card>
+      <v-card-title v-if="vote.categoryName">
+        {{ vote.categoryName }}
+      </v-card-title>
       <choice-list :choices="vote.choices"></choice-list>
     </v-card>
   </v-col>
